2f41ab9a33a523b8665df3e8cdc08dae6a449a0b
ht0m <hartht@gmail.com>
2021-10-26 16:55:55 +0200

    Student taks

--- src/main/java/day02/Mathematics.java
+++ src/main/java/day02/Mathematics.java
@@ -3,16 +3,17 @@ package day02;
 public class Mathematics {
 
     public boolean isPrime(int number) {
-        Boolean b = true;
-         for (int i = 3; i<number/2; i++)
-             if (number%i == 0) {
-                 b = false;
+
+        if (number<2) return false;
+         for (int i = 2; i<=number/2; i++)
+             if (number%i == 0)  {
+                 return false;
              }
-        return b;
+        return true;
     }
 
     public static void main(String[] args) {
-        int a = 13;
+        int a = 230;
         Mathematics maths = new Mathematics();
         System.out.println("Prím szám a "+ a+"? "+ maths.isPrime(a));
     }
